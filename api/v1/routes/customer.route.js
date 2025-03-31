ac6f290230629283baa3b962f180b8fcf45ee7e7
Huỳnh Phạm Long Triều <trieu7a2005@gmail.com>
2025-03-31 10:12:28 +0700

    connect mongodb

--- api/v1/routes/customer.route.js
+++ api/v1/routes/customer.route.js
@@ -2,8 +2,8 @@ const express = require("express")
 const controllers = require("../controllers/customer.controller")
 const verifyToken = require("../../../validate/verifyToken")
 const router =  express.Router()
-router.post("/reversation",verifyToken, controllers.postReversation);
-router.get("/reversation",verifyToken,controllers.getReversation);
+router.post("/reservation",verifyToken, controllers.postReversation);
+router.get("",verifyToken,controllers.getReversation);
 router.patch("/update/:id",verifyToken,controllers.updateTable)
 router.delete("/delete/:id",verifyToken,controllers.deleteTable)
 module.exports = router;
