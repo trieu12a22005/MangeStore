90f29ecb532b3c0c7863a89d210df74fab27f04d
Trung Nguyễn Đức <23521678@ms.uit.edu.vn>
2025-05-13 09:41:27 +0700

    push Schedule

---
+++ frontend/View/ScheduleView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using frontend.ViewModel;
+
+namespace frontend.View
+{
+    public partial class ScheduleView : UserControl
+    {
+        public ScheduleView()
+        {
+            InitializeComponent();
+            DataContext = new ScheduleViewModel("DefaultUser");
+        }
+    }
+}
